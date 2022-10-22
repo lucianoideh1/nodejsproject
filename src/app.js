79df5368a6f7be8d9f23ce5a7ac7c281d497459d
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-22 15:45:43 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const colors = require("colors")
+ 
+console.log("BOCA".bgBlue)
+console.log("BOCA".bgYellow)
+console.log("BOCA".bgBlue)
+
+console.log("GUMAGOD".bgRed)
+console.log("GUMAGOD".bgRed)
+console.log("GUMAGOD".bgRed)
+
+console.log("PEEPOKOI".bgMagenta)
+console.log("PEEPOKOI".bgMagenta)
+console.log("PEEPOKOI".bgMagenta)
+
+console.log("RAINBOW".rainbow)
